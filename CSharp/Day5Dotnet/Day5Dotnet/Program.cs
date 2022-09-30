fcad9c7c583987fb22cc59ab74cc37d6d2e44c97
BanurekhaV <rekhasairam@hotmail.com>
2022-09-30 13:29:39 +0530

    properties

--- CSharp/Day5Dotnet/Day5Dotnet/Program.cs
+++ CSharp/Day5Dotnet/Day5Dotnet/Program.cs
@@ -12,13 +12,14 @@ namespace Day5Dotnet
         static string CompanyAddress="Bangalore"; //static variable
         readonly string CompanyName = "Infinite"; //declaring and initializing
         readonly bool Limited; // initialized in the constructor
+        const float PI=5.14f; // for const, need to assign values at the time of decl. itself
         
         //constructor
         public Program(char c, bool f)
         {
             data = c;
             Limited = f;
-           // CompanyName = "ABC Corporation";
+            // CompanyName = "ABC Corporation";
         }
 
         public Program(bool b, string compname)
@@ -34,11 +35,13 @@ namespace Day5Dotnet
             Console.WriteLine("The Company is currently registered at " + " " + CompanyAddress);
             Console.WriteLine(p.data + " " +p.CompanyName+ " "+p.Limited);
             p.data = '1';
-           // p.Limited = false;
-           // p.CompanyName = "MyCompany";
+            // p.Limited = false; not possible to change
+            // p.CompanyName = "MyCompany"; not possible to change
             Program p1 = new Program(false, "Abc Co.");
             p1.data = 'B';
-           // p1.CompanyName = "New Co.";
+            // p1.CompanyName = "New Co.";
+            Console.WriteLine(Program.PI);
+           // Program.PI = 3.14f; not possible to change
             Console.Read();
         }
     }
---
+++ CSharp/Day5Dotnet/Day5Dotnet/PropertiesEg.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5Dotnet
+{
+    class Student
+    {
+        private string RollNo = "N.A.";
+        private string Name = "Dont Know";
+        private int age = 0;
+
+        //declare property for RollNo
+        public string Code
+        {
+            get { return RollNo; }
+            set { if(String.IsNullOrEmpty(value))
+                    RollNo = "Invaliddata"; }
+        }
+
+        //declare property for Name
+        public string _Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        //declare property for Age
+        public int Age
+        {
+            get { return age; }  //to read
+            set { age = value; } // to write
+        }
+    }
+    class PropertiesEg
+    {
+        static void Main()
+        {
+            Student student = new Student();
+            Console.WriteLine(student.Code + " " +student._Name + " " + student.Age);
+            student.Code = null;
+            student._Name = "JamesBond";
+            student.Age =65;
+            Console.WriteLine(student.Code + " " + student._Name + " " + student.Age);
+          //  Console.WriteLine("Enter Code, Name and Age :");
+          ////  student.Code = Console.ReadLine();
+          //  student._Name = Console.ReadLine();
+          //  student.Age = Convert.ToInt32(Console.ReadLine());
+          //  Console.WriteLine(student.Code + " " + student._Name + " " + student.Age);
+            Console.Read();
+        }
+    }
+}
