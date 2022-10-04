7bbe59b71c65b079ba43f6524dd692d429521cc6
BanurekhaV <rekhasairam@hotmail.com>
2022-10-04 16:16:48 +0530

    interface

---
+++ CSharp/Day7DotNet/Day7DotNet/AbstractEg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7DotNet
+{
+   abstract class Shapeclass
+    {
+        //declaring abstract method with asbtract modifier, without definition
+          abstract public int Area();
+
+        //non abstract method with definition
+        public void Display()
+        {
+            Console.WriteLine("Hi I am Abstract class");
+        }
+    }
+
+    class SquareClass : Shapeclass
+    {
+        int side;
+        public SquareClass(int n)
+        {
+            side = n;
+        }
+        public override int Area()
+        {
+            return side * side;
+        }
+    }
+    class AbstractEg 
+    {
+        static void Main()
+        {
+            Shapeclass sc = new SquareClass(4);
+            Console.WriteLine(sc.Area());
+            sc.Display();
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day7DotNet/Day7DotNet/ICustomer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7DotNet
+{
+    interface ICustomer   //prefix I for interface
+    {
+        void Print();  //only decl.
+        int EvenNo(int x);
+    }
+
+    interface ICustomer1
+    {
+        void Show();
+    }
+   
+    class Customer : ICustomer,ICustomer1
+    {
+        public void Print()
+        {
+            Console.WriteLine("ICustomer Interface Method..");
+        }
+        public int EvenNo(int a)
+        {
+            if (a % 2 == 0)
+                return a;
+            else
+                return 0;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("ICustomer Show Implementation");
+        }
+    }
+    class Tester
+    {
+        static void Main()
+        {
+            Customer c = new Customer();
+            c.Print();
+            Console.Read();
+           // ICustomer ic=new Customer();
+        }
+    }
+}
