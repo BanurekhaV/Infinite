54978503e008a60e0eaad3234e87bbc7c0a2aaeb
BanurekhaV <rekhasairam@hotmail.com>
2022-10-06 13:34:10 +0530

    exceptions

---
+++ CSharp/Day8Dotnet/Day8Dotnet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            try
+            {
+                Console.WriteLine("Enter 2 nos :");
+                a = Convert.ToInt32(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                c = a / b;
+                Console.WriteLine(c);
+                Console.Read();
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+                Console.WriteLine("Please enter only numbers");
+                Console.Read();
+            }          
+
+        }
+    }
+}
