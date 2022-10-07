643ee6fcb478fc75beecc729dccabfd9a2f1aba5
BanurekhaV <rekhasairam@hotmail.com>
2022-10-07 13:29:14 +0530

    collection

--- CSharp/Day8Dotnet/Day8Dotnet/GarbageCollectioneg.cs
+++ CSharp/Day8Dotnet/Day8Dotnet/GarbageCollectioneg.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 
 namespace Day8Dotnet
 {
-    class GarbageCollectioneg
+    class GarbageCollectioneg : IDisposable
     {
+        string s;
+        int i;
+        float f;
+        DateTime dt;
         static void Main()
         {
-            GarbageCollectioneg obj = new GarbageCollectioneg();
+            GarbageCollectioneg obj;
+            obj= new GarbageCollectioneg();
             Console.WriteLine("The number of generations in GC are :{0}",GC.MaxGeneration);
             Console.WriteLine("The generation number of the object is :" + GC.GetGeneration(obj));
-            Console.WriteLine("The totral Memory :"+ GC.GetTotalMemory(false));
+            Console.WriteLine("The total Memory :"+ GC.GetTotalMemory(false));
             Console.WriteLine("GC collection in Gen 0 is :" + GC.CollectionCount(0));
+            obj.Dispose();
             Console.Read();
         }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Finalize method is called");            
+        }        
     }
 
 }
---
+++ CSharp/Day9Dotnet/Day9Dotnet/CollectionEg.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+
+namespace Day9Dotnet
+{
+    class CollectionEg
+    {
+        public void ArrayListEg()
+        {
+            //ArrayList Collection can take different data types
+            ArrayList arrlist = new ArrayList();
+            arrlist.Add("Sampreeth");
+            arrlist.Add(10);
+            arrlist.Add(true);
+            arrlist.Add(35.45f);
+            arrlist.Add('S');
+            arrlist.Add(55.50);
+
+            foreach(var val in arrlist)
+            {
+                Console.WriteLine(val);
+            }
+
+            //insert a value at an index
+            arrlist.Insert(2, "Rama");
+           // arrlist.Remove(10); //or
+            arrlist.RemoveAt(1);
+            Console.WriteLine("--------------");
+            foreach (var val in arrlist)
+            {
+                Console.WriteLine(val);
+            }
+            Console.WriteLine("----------");
+            Console.WriteLine(arrlist.Capacity);
+            //another arraylist object
+            ArrayList newlist = new ArrayList();
+            newlist.Add(1);
+            newlist.Add(8);
+            newlist.Add(19);
+            newlist.Add(6);
+            newlist.Add(4);
+
+            arrlist.InsertRange(0,newlist);
+            Console.WriteLine("----after adding a range of values----");
+            Console.WriteLine(arrlist.Count);
+            Console.WriteLine(arrlist.Capacity);
+            
+            foreach (var val in arrlist)
+            {
+                Console.WriteLine(val);
+            }
+            Console.WriteLine("************");
+            foreach (int i in newlist)
+            {
+                Console.WriteLine(i);
+            }
+            newlist.Sort();
+            foreach(int i in newlist)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public static void Main()
+        {
+            CollectionEg ceg = new CollectionEg();
+            ceg.ArrayListEg();
+            Console.Read();
+        }
+    }
+}
