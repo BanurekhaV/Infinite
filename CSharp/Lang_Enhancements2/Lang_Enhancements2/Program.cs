7f42c198d4689c6ca0344b0de7dc3a12ac84920e
BanurekhaV <rekhasairam@hotmail.com>
2022-10-13 18:01:54 +0530

    Enhancements3

--- CSharp/Lang_Enhancements2/Lang_Enhancements2/Program.cs
+++ CSharp/Lang_Enhancements2/Lang_Enhancements2/Program.cs
@@ -25,6 +25,59 @@ namespace Lang_Enhancements2
             ESal = 50000;
             City = "Pune";
         }
+        #region
+        public void DictionaryInitializers()
+        {
+            //before 6.0 we were initializing as below
+            Dictionary<string, string> emp = new Dictionary<string, string>()
+            {
+                {"Rajesh","Emp100" },
+                {"Suresh","Emp009" },
+                {"Mukesh" ,"Emp005"}
+            };
+            //with 6.0 we can initialize as below
+            Dictionary<string, string> empdir = new Dictionary<string, string>()
+            {
+                ["Varsha"] = "Emp001",
+                ["Sairoopa"] = "Emp003",
+                ["Ramya"] = "Emp001",
+            };
+            Console.WriteLine("List of Employees");
+            foreach (KeyValuePair<string, string> kvp in emp)
+            {
+                Console.WriteLine($"Name : \t{kvp.Key}\nEmpId :\t{kvp.Value}");
+            }
+        }
+        #endregion
+        public void ExceptionFilters()
+        {
+            try
+            {
+                int n = 10;
+                int res = 10 / n;
+                int[] arr = new int[3];
+                arr[5] = 20;
+                Console.WriteLine("Everything went off well..");
+            }
+            catch(DivideByZeroException)when(DateTime.Now.DayOfWeek==DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("It is Thursday Today");
+            }
+            catch(DivideByZeroException de)when(de.GetType().ToString()==
+            "System.DivideByZeroException")
+            {
+                DoSomething();
+            }
+            catch(IndexOutOfRangeException)when(DateTime.Now.DayOfWeek ==DayOfWeek.Wednesday)
+            {
+                Console.WriteLine("Handling exception when it is not a wednesday");
+            }
+        }
+
+        public static void DoSomething()
+        {
+            Console.WriteLine("A new Job is getting Done...");
+        }
 
     }
     class Program :Employee
@@ -32,17 +85,17 @@ namespace Lang_Enhancements2
         static void Main(string[] args)
         {
             Employee e = new Employee();
-            Console.WriteLine(e.Ename + e.ESal+e.City);
-            e.Ename = "Bennett";
-            e.propertySetters();
-            //e.City = "Pune"; //not possible
+            //Console.WriteLine(e.Ename + e.ESal+e.City);
+            //e.Ename = "Bennett";
+            //e.propertySetters();
+            ////e.City = "Pune"; //not possible
             
             Program p = new Program();
-            p.City = "Mumbai";
-            Console.WriteLine(e.Ename + e.ESal+e.City);
+            //p.City = "Mumbai";
+            //Console.WriteLine(e.Ename + e.ESal+e.City);
+            //    p.DictionaryInitializers();
+           e.ExceptionFilters();
             Console.Read();
         }
-    }
-
-    
+    }    
 }
