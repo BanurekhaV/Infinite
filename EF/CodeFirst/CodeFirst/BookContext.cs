cdef5caa445ac419f73e27f5199419119c8fd9e7
BanurekhaV <rekhasairam@hotmail.com>
2022-11-03 13:50:25 +0530

    code first

---
+++ EF/CodeFirst/CodeFirst/BookContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class BookContext:DbContext
+    {
+        public BookContext():base("name=Bookcontext")
+        { }
+
+        //include all models as DbSet Objects
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Publishers> Publisher { get; set; }
+        public DbSet<Authors> Author { get; set; }
+    }
+}
---
+++ EF/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    class Program
+    {
+        static BookContext bc = new BookContext();
+        static void ShowBooks()
+        {
+            var book = from b in bc.Books
+                       select b;
+
+            foreach(var item in book)
+            {
+                Console.WriteLine(item.BookId + " " + item.BookName);
+            }
+        }
+
+        static void UpdateBooks()
+        {
+            var bk = bc.Books.FirstOrDefault(b => b.BookId == 3);
+
+            bk.BookName = "Intro to Dot Net Programming";
+            bc.SaveChanges();
+            //for delete
+            //bc.Books.Remove(bk);
+            //bc.SaveChanges();          
+            
+        }
+        static void Main(string[] args)
+        {
+            UpdateBooks();
+            ShowBooks();
+            //using(var db= new BookContext())
+            //{
+            //    db.Books.Add(new Book { BookName = "Introduction to EF" });
+            //    db.SaveChanges();
+
+            //    foreach(var bk in db.Books)
+            //    {
+            //        Console.WriteLine(bk.BookId + " "+ bk.BookName);
+            //    }
+            //}
+            Console.Read();
+        }
+    }
+}
