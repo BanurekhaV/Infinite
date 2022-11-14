1f39ebb7961fe1a2f3063c4cd29a14ae617498d9
BanurekhaV <rekhasairam@hotmail.com>
2022-11-14 11:41:53 +0530

    postback

---
+++ ASP/Statemanagement/Statemanagement/PostBack.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PostBack.aspx.cs" Inherits="Statemanagement.PostBack" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txt1" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnclick" runat="server" Text="Clicks Count" OnClick="btnclick_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP/Statemanagement/Statemanagement/PostBack.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Statemanagement
+{
+    public partial class PostBack : System.Web.UI.Page
+    {
+        int ClicksCount = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                txt1.Text = "0";
+            }
+
+        }
+
+        protected void btnclick_Click(object sender, EventArgs e)
+        {
+            /* step 1
+            ClicksCount = ClicksCount + 1;
+            txt1.Text = ClicksCount.ToString();*/
+            //step 2 using viewstate variables
+            //if(ViewState["Clicks"]!=null)
+            //{
+            //    ClicksCount = (int)ViewState["Clicks"] + 1;
+            //}
+            //txt1.Text = ClicksCount.ToString();
+            //ViewState["Clicks"] = ClicksCount;
+
+            //step 3 without creating view state variable, since 
+            //every asp control has a viewstate variable that can maintain the state
+            ClicksCount = Convert.ToInt32(txt1.Text)+1;
+            txt1.Text = ClicksCount.ToString();
+        }
+    }
+}
