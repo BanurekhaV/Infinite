1022d0c3381bf947f38fa0d3a848c39386dfd02d
BanurekhaV <rekhasairam@hotmail.com>
2022-11-15 16:17:45 +0530

    webservices_2

--- ASP/WebServices/WebClient/ClientWebForm.aspx
+++ ASP/WebServices/WebClient/ClientWebForm.aspx
@@ -10,9 +10,9 @@
     <form id="form1" runat="server">
         Enter Your Name : <asp:TextBox ID="txtname" runat="server"></asp:TextBox><br />
         Enter a Real Number :<asp:TextBox ID="txtnum" runat="server"></asp:TextBox><br /><br />
-        <asp:Button ID="hellobtn" runat="server" Text="ForHelloMessage" />
+        <asp:Button ID="hellobtn" runat="server" Text="ForHelloMessage" OnClick="hellobtn_Click" />
         <asp:Button ID="msgbtn" runat="server" Text="For Message" OnClick="msgbtn_Click" />
-        <asp:Button ID="sqbtn" runat="server" Text="ForSquare" />
+        <asp:Button ID="sqbtn" runat="server" Text="ForSquare" OnClick="sqbtn_Click" />
         <br /><br />
         <asp:Label ID="lbldata" runat="server"></asp:Label>
     </form>
--- ASP/WebServices/WebClient/ClientWebForm.aspx.cs
+++ ASP/WebServices/WebClient/ClientWebForm.aspx.cs
@@ -9,15 +9,27 @@ namespace WebClient
 {
     public partial class ClientWebForm : System.Web.UI.Page
     {
+        MyServicesReference.MyServicesSoapClient client = new MyServicesReference.MyServicesSoapClient();
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
         protected void msgbtn_Click(object sender, EventArgs e)
-        {
-            MyServicesReference.MyServicesSoapClient client = new MyServicesReference.MyServicesSoapClient();
+        {           
             lbldata.Text=client.Message(txtname.Text);
+        }
+
+        protected void hellobtn_Click(object sender, EventArgs e)
+        {
+            //MyServicesReference.MyServicesSoapClient client = new MyServicesReference.MyServicesSoapClient();
+            lbldata.Text=client.HelloWorld();
+        }
+
+        protected void sqbtn_Click(object sender, EventArgs e)
+        {
+            float res = client.Squareroot(float.Parse(txtnum.Text));
+            lbldata.Text = res.ToString();
         }
     }
 }
