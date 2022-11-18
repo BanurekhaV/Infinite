dedb9a47c4fb93cf84393786850af4007e495aee
BanurekhaV <rekhasairam@hotmail.com>
2022-11-18 13:34:58 +0530

    forms collection

---
+++ MVC/Day3EFPrj/Day3EFPrj/Controllers/ShippersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day3EFPrj.Models;
+
+namespace Day3EFPrj.Controllers
+{
+    public class ShippersController : Controller
+    {
+        NorthwindEntities ne = new NorthwindEntities();
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            return View(ne.Shippers.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult CreateShipper()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateShipper(Shipper s)
+        {
+            ne.Shippers.Add(s);
+            ne.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
