8fb0261253fbb45cd58fdc050d023b9c3cb09e8f
BanurekhaV <rekhasairam@hotmail.com>
2022-11-19 16:32:46 +0530

    Helpers

---
+++ MVC/HtmlHelpersPrj/HtmlHelpersPrj/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace HtmlHelpersPrj.Models
+{
+    public class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+    }
+}
