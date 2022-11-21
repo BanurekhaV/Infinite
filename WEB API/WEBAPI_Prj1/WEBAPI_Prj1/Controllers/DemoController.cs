4c4811505f489d0cec17787be8bb7bca5a7b767e
BanurekhaV <rekhasairam@hotmail.com>
2022-11-21 13:44:13 +0530

    attributerouting

--- WEB API/WEBAPI_Prj1/WEBAPI_Prj1/Controllers/DemoController.cs
+++ WEB API/WEBAPI_Prj1/WEBAPI_Prj1/Controllers/DemoController.cs
@@ -25,18 +25,43 @@ namespace WEBAPI_Prj1.Controllers
             return continents[id];
         }
 
-        //Post 
+        //Post using an model Object
         public IEnumerable<string> Post([FromBody] Sample s)
         {
             continents.Add(s.name);
             //continents.Add(val);
             return continents;
         }
+
+        
         [HttpPost]
-        public IEnumerable<string>Addstring([FromUri] string val)
+        public IEnumerable<string> Addstring([FromUri] string val)
         {
             continents.Add(val);
             return continents;
+        }
+
+        //Put :api/demo/id
+        public IEnumerable<string>Put(int id,[FromUri] string v)
+        {
+            continents[id-1] = v;
+            return continents;
+        }
+
+        //delete :api/demo/id
+        //this gives the contents back after deletion
+
+        //public IEnumerable<string>Delete(int id)
+        //{
+        //    continents.RemoveAt(id - 1);
+        //    return continents;
+        //}
+
+        //this does not reflect delete, because the delete
+        //method is void
+        public void Delete(int id)
+        {
+            continents.RemoveAt(id - 1);
         }
     }
 }
---
+++ WEB API/WEBAPI_Prj1/WEBAPI_Prj1/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPI_Prj1.Models;
+
+namespace WEBAPI_Prj1.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        static List<Users> userlist = new List<Users>()
+        {
+            new Users{ID=1,Name="Rohan", Role="Operations"},
+            new Users{ID=2,Name="Vikram", Role="Admin"},
+            new Users{ID=3,Name="Rahul", Role="User"},
+        };
+
+        [HttpGet]
+        [Route("General")]
+        public IEnumerable<Users>Get()
+        {
+            return userlist;
+        }
+
+        //Get using HttpResponseMessage object
+        [HttpGet]
+        [Route("UserHttp")]
+        public HttpResponseMessage GetUserList()
+        {
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, userlist);
+            return response;
+        }
+
+
+    }
+}
